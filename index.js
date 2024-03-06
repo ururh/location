ae7226097568aca52071360a4c39119ca1410cfc
Inna Zlotska <58848421+ururh@users.noreply.github.com>
2024-03-06 16:56:22 +0200

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const button = document.querySelector("button");
 button.addEventListener("click", () => {
   navigator.geolocation.getCurrentPosition(position => {
     const { latitude, longitude } = position.coords;
-    const url = `http://nominatim.openstreetmap.org/reverse?format=json&lat=${latitude}&lon=${longitude}`;
+    const url = `https://nominatim.openstreetmap.org/reverse?format=json&lat=${latitude}&lon=${longitude}`;
     fetch(url).then(res => res.json()).then(data => {
       console.table(data.address);
     }).catch(() => {
